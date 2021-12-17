ad72999828a21f2108b7bf1df6aec34db9691983
EC2 Default User <ec2-user@ip-172-31-9-150.ap-northeast-2.compute.internal>
2021-12-17 20:10:22 +0000

    express

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+
+rts = {
+		// Example setup for your project:
+	// 	// The entry module that requires or imports the rest of your project.
+	// 		// Must start with `./`!
+ 			entry: './index.js',
+	// 				// Place output files in `./dist/my-app.js`
+ 					output: {
+ 							path: __dirname + '/dist',
+ 									filename: 'my-app.js',
+ 										},
+							};
